0402aa9d410b9af360e1416c4be7eff0d68f9cf7
ejBlack12 <josueelmer1234@gmail.com>
2025-03-01 07:43:19 -0600

    el resto de ventanas
    En este commit se carga las ventanas restantes

---
+++ Examen/ExamenGrupo5/VentanaVentas.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ExamenGrupo5
+{
+    partial class VentanaVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelPrincipal = new System.Windows.Forms.Panel();
+            this.PanelArriba = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelDerecha = new System.Windows.Forms.Panel();
+            this.labelVentas = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBoxAccesos = new System.Windows.Forms.GroupBox();
+            this.buttonVolver = new System.Windows.Forms.Button();
+            this.buttonGestion = new System.Windows.Forms.Button();
+            this.buttonReporte = new System.Windows.Forms.Button();
+            this.buttonMostrar = new System.Windows.Forms.Button();
+            this.panelDerecha.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBoxAccesos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelPrincipal
+            // 
+            this.panelPrincipal.BackColor = System.Drawing.Color.Snow;
+            this.panelPrincipal.Location = new System.Drawing.Point(264, 79);
+            this.panelPrincipal.Margin = new System.Windows.Forms.Padding(4);
+            this.panelPrincipal.Name = "panelPrincipal";
+            this.panelPrincipal.Size = new System.Drawing.Size(815, 480);
+            this.panelPrincipal.TabIndex = 17;
+            // 
+            // PanelArriba
+            // 
+            this.PanelArriba.BackColor = System.Drawing.Color.MidnightBlue;
+            this.PanelArriba.Location = new System.Drawing.Point(264, 1);
+            this.PanelArriba.Margin = new System.Windows.Forms.Padding(4);
+            this.PanelArriba.Name = "PanelArriba";
+            this.PanelArriba.Size = new System.Drawing.Size(815, 79);
+            this.PanelArriba.TabIndex = 16;
+            // 
+            // panelDerecha
+            // 
+            this.panelDerecha.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panelDerecha.Controls.Add(this.labelVentas);
+            this.panelDerecha.Controls.Add(this.pictureBox1);
+            this.panelDerecha.Controls.Add(this.groupBoxAccesos);
+            this.panelDerecha.Location = new System.Drawing.Point(-4, -3);
+            this.panelDerecha.Margin = new System.Windows.Forms.Padding(4);
+            this.panelDerecha.Name = "panelDerecha";
+            this.panelDerecha.Size = new System.Drawing.Size(273, 566);
+            this.panelDerecha.TabIndex = 15;
+            // 
+            // labelVentas
+            // 
+            this.labelVentas.AutoSize = true;
+            this.labelVentas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelVentas.ForeColor = System.Drawing.Color.Goldenrod;
+            this.labelVentas.Location = new System.Drawing.Point(97, 126);
+            this.labelVentas.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelVentas.Name = "labelVentas";
+            this.labelVentas.Size = new System.Drawing.Size(67, 20);
+            this.labelVentas.TabIndex = 3;
+            this.labelVentas.Text = "Ventas";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(84, 39);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 60);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBoxAccesos
+            // 
+            this.groupBoxAccesos.Controls.Add(this.buttonVolver);
+            this.groupBoxAccesos.Controls.Add(this.buttonGestion);
+            this.groupBoxAccesos.Controls.Add(this.buttonReporte);
+            this.groupBoxAccesos.Controls.Add(this.buttonMostrar);
+            this.groupBoxAccesos.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.groupBoxAccesos.ForeColor = System.Drawing.Color.Goldenrod;
+            this.groupBoxAccesos.Location = new System.Drawing.Point(29, 175);
+            this.groupBoxAccesos.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBoxAccesos.Name = "groupBoxAccesos";
+            this.groupBoxAccesos.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBoxAccesos.Size = new System.Drawing.Size(227, 346);
+            this.groupBoxAccesos.TabIndex = 0;
+            this.groupBoxAccesos.TabStop = false;
+            this.groupBoxAccesos.Text = "Accesos Rápidos";
+            // 
+            // buttonVolver
+            // 
+            this.buttonVolver.BackColor = System.Drawing.Color.Goldenrod;
+            this.buttonVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.buttonVolver.ForeColor = System.Drawing.Color.Black;
+            this.buttonVolver.Location = new System.Drawing.Point(40, 273);
+            this.buttonVolver.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonVolver.Name = "buttonVolver";
+            this.buttonVolver.Size = new System.Drawing.Size(149, 33);
+            this.buttonVolver.TabIndex = 4;
+            this.buttonVolver.Text = "Volver";
+            this.buttonVolver.UseVisualStyleBackColor = false;
+            // 
+            // buttonGestion
+            // 
+            this.buttonGestion.BackColor = System.Drawing.Color.Goldenrod;
+            this.buttonGestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.buttonGestion.ForeColor = System.Drawing.Color.Black;
+            this.buttonGestion.Location = new System.Drawing.Point(40, 121);
+            this.buttonGestion.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonGestion.Name = "buttonGestion";
+            this.buttonGestion.Size = new System.Drawing.Size(149, 33);
+            this.buttonGestion.TabIndex = 3;
+            this.buttonGestion.Text = "Gestión";
+            this.buttonGestion.UseVisualStyleBackColor = false;
+            // 
+            // buttonReporte
+            // 
+            this.buttonReporte.BackColor = System.Drawing.Color.Goldenrod;
+            this.buttonReporte.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.buttonReporte.ForeColor = System.Drawing.Color.Black;
+            this.buttonReporte.Location = new System.Drawing.Point(40, 197);
+            this.buttonReporte.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonReporte.Name = "buttonReporte";
+            this.buttonReporte.Size = new System.Drawing.Size(149, 33);
+            this.buttonReporte.TabIndex = 1;
+            this.buttonReporte.Text = "Reporte";
+            this.buttonReporte.UseVisualStyleBackColor = false;
+            // 
+            // buttonMostrar
+            // 
+            this.buttonMostrar.BackColor = System.Drawing.Color.Goldenrod;
+            this.buttonMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.buttonMostrar.ForeColor = System.Drawing.Color.Black;
+            this.buttonMostrar.Location = new System.Drawing.Point(40, 49);
+            this.buttonMostrar.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonMostrar.Name = "buttonMostrar";
+            this.buttonMostrar.Size = new System.Drawing.Size(149, 33);
+            this.buttonMostrar.TabIndex = 0;
+            this.buttonMostrar.Text = "Mostrar";
+            this.buttonMostrar.UseVisualStyleBackColor = false;
+            // 
+            // VentanaVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 550);
+            this.Controls.Add(this.panelPrincipal);
+            this.Controls.Add(this.PanelArriba);
+            this.Controls.Add(this.panelDerecha);
+            this.Name = "VentanaVentas";
+            this.Text = "VentanaVentas";
+            this.panelDerecha.ResumeLayout(false);
+            this.panelDerecha.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBoxAccesos.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPrincipal;
+        private System.Windows.Forms.FlowLayoutPanel PanelArriba;
+        private System.Windows.Forms.Panel panelDerecha;
+        private System.Windows.Forms.Label labelVentas;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBoxAccesos;
+        private System.Windows.Forms.Button buttonVolver;
+        private System.Windows.Forms.Button buttonGestion;
+        private System.Windows.Forms.Button buttonReporte;
+        private System.Windows.Forms.Button buttonMostrar;
+    }
+}
