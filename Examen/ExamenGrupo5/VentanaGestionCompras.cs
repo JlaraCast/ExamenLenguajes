d71fbceac8d759db86a44085a69b3d4f3ec431ea
Kev <ualvaradokevin2005@gmail.com>
2025-03-01 20:31:45 -0600

    Se agrega la ventana sin funciones

---
+++ Examen/ExamenGrupo5/VentanaGestionCompras.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamenGrupo5
+{
+    public partial class VentanaGestionCompras : Form
+    {
+        public VentanaGestionCompras()
+        {
+            InitializeComponent();
+        }
+
+        private void label_Estado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
